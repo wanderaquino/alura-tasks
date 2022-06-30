5144a1566b908145c4b8d843a7211daeebbeb97d
Wander Aquino <wander.aquino@gmail.com>
2022-06-30 01:36:08 -0300

    Add state for render new tasks

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,27 @@ import {Form} from "./components/Form";
 import {List} from "./components/List";
 import style from "./App.module.scss";
 import { Stopwatch } from "./components/Stopwatch";
+import { useState } from "react";
 
 function App() {
 
+  const [tasks, setTasks] = useState( [
+        {
+            title: "VueJs",
+            time: "01:00:00"
+        }
+      ]
+  );
+
+  function addNewTask({title, time} : {title: string, time: string}) {
+    const taskState = tasks;
+    setTasks([...taskState, {title, time}]);
+  }
+
   return (
     <div className={style.AppStyle}>
-      <Form />
-      <List />
+      <Form addTask={addNewTask}/>
+      <List tasks={tasks}/>
       <Stopwatch />
     </div>
   )
