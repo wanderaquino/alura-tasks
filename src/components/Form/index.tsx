164b4c0254517605145109a081a4011cee7480fd
Wander Aquino <wander.aquino@gmail.com>
2022-06-30 01:38:24 -0300

    Add state to control the form before include new task

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,10 +1,34 @@
 import React from "react";
 import {Button} from "../Button/index";
 import style from "./style.module.scss";
+import {useState} from "react";
 
-export function Form () {
+type FormProps = {
+    addTask: ({} : {title: string, time: string})=>void
+}
+export function Form ({addTask} : FormProps) {
+
+    const [title, setTitle] = useState<string>();
+    const [time, setTime] = useState<string>();
+
+    function handleChangeTitle(title : string){
+        setTitle(title);
+    };
+
+    function handleCHangeTime(time: string) {
+        setTime(time);
+    }
+
+    function setNewTask(e:any){
+        e.preventDefault();
+        if(title === undefined || time === undefined) {
+            return;
+        }
+        addTask({title, time});
+    };
+
     return (
-        <form className={style.novaTarefa}>
+        <form className={style.novaTarefa} onSubmit={e => setNewTask(e)}>
             <div className={style.inputContainer}>
                 <label htmlFor="task">Adicione um novo estudo</label>
                 <input 
@@ -12,7 +36,8 @@ export function Form () {
                     name="task" 
                     id="task" 
                     placeholder="O que você quer estudar hoje?" 
-                    required></input>
+                    required
+                    onChange={e => handleChangeTitle(e.target.value)}></input>
             </div>
             <div className={style.inputContainer}>
                 <label htmlFor="time">Tempo</label>
@@ -23,9 +48,10 @@ export function Form () {
                     id="time"
                     min="00:00:00"
                     max="01:30:00"
-                    required></input>
+                    required
+                    onChange={e => handleCHangeTime(e.target.value)}></input>
             </div>
-            <Button textButton="Adicionar" />
+            <Button textButton="Adicionar"/>
         </form>
     )
 }
