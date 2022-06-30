7f711778d24a6cfce9e3b43fb91241483c721ba3
Wander Aquino <wander.aquino@gmail.com>
2022-06-30 00:00:02 -0300

    Use ListItem component

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,18 +1,14 @@
-import React from "react"
+import { ListItem } from "./ListItem";
+import style from "./style.module.scss";
 
 export function List () {
     return (
-        <aside>
+        <aside className={style.listaTarefas}>
             <h2>Estudos do dia</h2>
             <ul>
-                <li>
-                    <h3>React</h3>
-                    <span>02:00:00</span>
-                </li>
-                <li>
-                    <h3>Javascript</h3>
-                    <span>01:00:00</span>
-                </li>
+                <ListItem title="React" time="02:00:00" />
+                <ListItem title="VueJS" time="01:00:00" />
+                <ListItem title="Javascript" time="03:00:00" />
             </ul>
         </aside>
     )
