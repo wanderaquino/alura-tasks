b22255801d75a520935721bbf789d70b57454bc5
Wander Aquino <wander.aquino@gmail.com>
2022-06-30 00:00:19 -0300

    Render stopwatch component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import {Form} from "./components/Form";
 import {List} from "./components/List";
+import style from "./App.module.scss";
+import { Stopwatch } from "./components/Stopwatch";
+
 function App() {
 
   return (
-    <div className="App">
+    <div className={style.AppStyle}>
       <Form />
       <List />
+      <Stopwatch />
     </div>
   )
 }
