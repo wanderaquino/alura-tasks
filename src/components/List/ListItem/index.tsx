baa21a23c15299d2d80be32019ef616e2660fca8
Wander Aquino <wander.aquino@gmail.com>
2022-07-03 23:14:21 -0300

    Import and set TaskProps type

--- src/components/List/ListItem/index.tsx
+++ src/components/List/ListItem/index.tsx
@@ -1,11 +1,7 @@
 import style from "./style.module.scss";
+import { TaskProps } from "../../../types/types";
 
-type ListItemProps = {
-    title: string,
-    time: string
-}
-
-export function ListItem ({title, time} : ListItemProps) {
+export function ListItem ({id, title, time, isSelected, selectFunction} : TaskProps) {
     return (
         <>
         <li className={`${style.item} ${isSelected ? style.itemSelecionado : ''}`}
