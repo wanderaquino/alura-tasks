606061058a73da77323d1ae8b5cf43d56a233d67
Wander Aquino <wander.aquino@gmail.com>
2022-07-03 23:15:50 -0300

    Check before render a task list

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -11,13 +11,27 @@ interface ListProps {
 export function List ({tasks} : ListProps) {
     return (
         <aside className={style.listaTarefas}>
+        {
+        tasks !== undefined && tasks.length > 0 ?
+        <>
             <h2>Estudos do dia</h2>
             <ul>
                 {
-                    tasks.map((task, index) => 
-                    <ListItem key={index} title={task.title} time={task.time}></ListItem>)
+                tasks.map((task) => 
+                <ListItem 
+                    key={task.id}
+                    id={task.id}
+                    title={task.title} 
+                    time={task.time} 
+                    isSelected={task.isSelected}
+                    selectFunction={selectFunction} 
+                />)
                 }
             </ul>
+        </>
+        : 
+        <h2>Você não possui atividades cadastradas ainda. Que tal adicionar uma nova?</h2>
+        }
         </aside>
     )
 };
