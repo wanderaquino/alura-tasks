c50474cd820bc3b86d9ef30f071afeefced53d80
Wander Aquino <wander.aquino@gmail.com>
2022-07-04 01:20:47 -0300

    Renders time dinamically

--- src/components/Stopwatch/Watch/index.tsx
+++ src/components/Stopwatch/Watch/index.tsx
@@ -4,11 +4,11 @@ import {WatchProps} from "../../../types/types";
 export function Watch ({time} : WatchProps) {
     return (
         <>
-            <span className={style.relogioNumero}>0</span>
-            <span className={style.relogioNumero}>0</span>
-            <span className={style.relogioDivisao}>:</span>
-            <span className={style.relogioNumero}>0</span>
-            <span className={style.relogioNumero}>0</span>
+            <span className={style.relogioNumero}>{time[0]}</span>
+            <span className={style.relogioNumero}>{time[1]}</span>
+            <span className={style.relogioDivisao}>{time[2]}</span>
+            <span className={style.relogioNumero}>{time[3]}</span>
+            <span className={style.relogioNumero}>{time[4]}</span>
         </>
     )
 }
