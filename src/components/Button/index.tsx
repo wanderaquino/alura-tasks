24355ee26d2b19efbbd777affe968907eb58c519
Wander Aquino <wander.aquino@gmail.com>
2022-07-18 00:25:59 -0300

    Add toggle disabled button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,9 +2,9 @@ import style from "./style.module.scss";
 import {ButtonProps} from "../../types/types";
 
 
-export function Button ({textButton, type, onClickFunction} : ButtonProps) {
+export function Button ({textButton, type, onClickFunction, isDisabled=false} : ButtonProps) {
     return (
-        <button className={style.button} type={type} onClick={onClickFunction}>
+        <button className={`${style.button} ${isDisabled && style.buttonDisabled}`} type={type} onClick={onClickFunction} disabled={isDisabled}>
             {textButton}
         </button>
     )
