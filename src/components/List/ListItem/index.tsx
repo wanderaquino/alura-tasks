f8bc2478cae8685c44af3c1005d59221975da076
Wander Aquino <wander.aquino@gmail.com>
2022-07-17 21:44:07 -0300

    Include isFinished parameter for taskItem

--- src/components/List/ListItem/index.tsx
+++ src/components/List/ListItem/index.tsx
@@ -1,11 +1,11 @@
 import style from "./style.module.scss";
 import { SelectableTask } from "../../../types/types";
 
-export function ListItem ({id, title, time, isSelected, selectFunction} : SelectableTask) {
+export function ListItem ({id, title, time, isSelected, isFinished, selectFunction} : SelectableTask) {
     return (
         <>
-        <li className={`${style.item} ${isSelected ? style.itemSelecionado : ''}`}
-            onClick={() => selectFunction({id, title, time, isSelected})}>
+        <li className={`${style.item} ${isSelected ? style.itemSelecionado : ''} ${isFinished ? style.itemCompletado : ''}`}
+            onClick={() => selectFunction({id, title, time, isSelected, isFinished: false})}>
             <h3>{title}</h3>
             <span>{time}</span>
         </li>
