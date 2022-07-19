71b431a67e2b9db8a264f69e23ef6efcba62d59b
Wander Aquino <wander.aquino@gmail.com>
2022-07-18 22:28:17 -0300

    Apply conditionally disable style

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,7 +4,10 @@ import {ButtonProps} from "../../types/types";
 
 export function Button ({textButton, type, onClickFunction, isDisabled=false} : ButtonProps) {
     return (
-        <button className={`${style.button} ${isDisabled && style.buttonDisabled}`} type={type} onClick={onClickFunction} disabled={isDisabled}>
+        <button 
+            className={`${style.button} ${isDisabled && style.buttonDisabled}`} 
+            type={type} onClick={onClickFunction} 
+            disabled={isDisabled}>
             {textButton}
         </button>
     )
